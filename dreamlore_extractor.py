77b1407b1d79ab748bd9aa4909c77e4eedb4e977
lolbot-iichan <lolbot_iichan@mail.ru>
2016-10-19 01:48:21 +0300

    sys0.dfn of Onegin

--- dreamlore_extractor.py
+++ dreamlore_extractor.py
@@ -63,6 +63,33 @@ def encode_cnes_data(data):
         result += struct.pack('i',-16)
     return result
 
+def decode_sys0dfn_data(data):
+    result = ""
+    offset = 4
+    for i in range(struct.unpack('i',data[:4])[0]):
+        size = struct.unpack('i',data[offset:offset+4])[0]
+        name = data[offset+4:offset+4+size].decode("utf-16le")
+        offset += 4 + size
+        trig_type = struct.unpack('i',data[offset:offset+4])[0]
+        trig_type = "sys" if trig_type == 1 else "gal"
+        size = struct.unpack('i',data[offset+4:offset+8])[0]
+        value = data[offset+8:offset+8+size].decode("utf-16le")
+        offset += 8 + size
+        result += trig_type + " " + name + "=" + value + "\n"
+    return result
+
+def encode_sys0dfn_data(data):
+    lines = [l for l in data.split("\n") if l]
+    result = struct.pack('i',len(lines))
+    for line in lines:
+        trig_type = 1 if line[:3] == "sys" else 2
+        k,v = line[4:].split("=")
+        k,v = k.encode("utf-16le"), v.encode("utf-16le")
+        result += struct.pack('i',len(k)) + k
+        result += struct.pack('i',trig_type)
+        result += struct.pack('i',len(v)) + v
+    return result
+
 def encode_necro_save_data(data):
     result = ""
     data = dict([l.split("=") for l in data.split("\r\n") if "=" in l])
@@ -108,12 +135,14 @@ def usage():
     print "        cosmos <scn-file> with encoded scripts/persistent"
     print "        onegin <cnes-file> with encoded scripts"
     print "        onegin <pak-file> with packed pak data (+cnes converted)"
+    print "        onegin <sys0.dfn-file> with encoded persistent"
     print "    packs/encodes:"
     print "        necro  <folder> with unpacked pak data"
     print "        necro  <save-file> with readable sav data"
     print "        cosmos <folder> with unpacked pak/sav data"
     print "        cosmos <scene-file> with readable scripts/persistent"
     print "        onegin <nes-file> with readable scripts"
+    print "        onegin <sys0.persistent-file> with readable persistent"
 
 if  len(sys.argv) != 3 or sys.argv[1] not in ["necro","cosmos","onegin"]:
     usage()
@@ -152,6 +181,10 @@ if  os.path.isfile(path):
             write_files_data(".",[(path[:-4]+".cnes",encode_cnes_data(data))])
         elif path.lower().endswith(".pak"):
             write_files_data(path.split(".")[0],parse_zip_data(path))
+        elif path.lower().endswith("sys0.dfn"):
+            write_files_data(".",[(path[:-4]+".persistent",decode_sys0dfn_data(data))])
+        elif path.lower().endswith("sys0.persistent"):
+            write_files_data(".",[(path[:-11]+".dfn",encode_sys0dfn_data(data))])
         else:
             usage()
 
